b5311a1d05606daca0aaa291528bb2ad1782ec5e
Anusha <anushavysakh232@gmail.com>
2022-07-29 01:21:23 +0530

    StockManagement

---
+++ Account.java
@@ -0,0 +1,34 @@
+package com.stockmanagement.assignment;
+
+
+import java.util.Scanner;
+
+public class Account {
+
+	static Scanner sc = new Scanner(System.in);
+
+	private double balance;
+
+	public Account(double balance) {
+		super();
+		this.balance = balance;
+		// TODO Auto-generated constructor stub
+	}
+
+	public void debit(double balance) {
+		System.out.println("enter ammount to withdraw");
+		double withdraw = sc.nextDouble();
+		if (withdraw > balance)
+			System.out.println("withdraw exceeds the balance please enter correct ammount");
+		else
+			balance = balance - withdraw;
+		System.out.println("Remaining balance is " + balance);
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter the balance to mention in the account");
+		Account account = new Account(sc.nextDouble());
+		account.debit(account.balance);
+	}
+}
